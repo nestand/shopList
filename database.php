c90a095a3fed846f1dd7cc9726e5f9b03bb3a36b
Andrey NESTEROV <andrey.nesterov@laposte.net>
2021-04-28 12:24:17 +0200

    third commit

--- database.php
+++ database.php
@@ -1,51 +1,87 @@
 <?php
 //class qui gere la connexion BDD
 
-class database{
-
-    static $hostname = "localhost";
-    static $dbname = "listecourses";
-    static $userdb = "root";
-    static $mdpdb = "";
-    static $driverdb = "mysql";
-    static $connect;
+class database
+{
+    public static $hostname = "localhost";
+    public static $dbname = "listecourses";
+    public static $userdb = "root";
+    public static $mdpdb = "";
+    public static $driverdb = "mysql";
+    public static $connect;
 
     //connexion DB
 
-    static function createConnexion(){
-        if(empty(self::$connect)){
+    public static function createConnexion()
+    {
+        if (empty(self::$connect)) {
             self::$connect = new PDO(self::$driverdb.":host=".self::$hostname.";dbname=".self::$dbname, self::$userdb, self::$mdpdb);
         }
     }
 
-    //DB Query recup la liste
+    //DB Query recup produits
 
 
     static function getList()
     {
             self::createConnexion();
-        $sql = "SELECT nom FROM produit";
-        $data = self::$connect->query($sql);
-        return $data->fetchAll(PDO::FETCH_OBJ);
+            $sql = "SELECT nom FROM produit";
+            $data = self::$connect->query($sql);
+            return $data->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    // pour ajouter dans notre liste
+
+   
+  //selection d'un magasin
+   static function chooseShop()
+   {
+       self::createConnexion();
+           $sql = "SELECT nom FROM magasin";
+           $data = self::$connect->query($sql);
+           return $data->fetchAll(PDO::FETCH_ASSOC);
+      
+   }
+   
+   
+   /*
+   // pour ajouter dans notre liste
     static function addProd()
-    self::createConnexion();
-    $sql= "INSERT INTO produit(id, nom, prix_unit) VALUES ('', 'test','5euro')";    
-    if ($connect -> query($sql)===TRUE){
-        echo "ADD Success!"
-    }else{
-        echo "ERROR" . $sql . "<br>" . $connect->error; 
-    }
     
+    {
+        self::createConnexion();
+        $sql= "INSERT INTO produit(nom, prix_unit) VALUES ('test6',5)";
+        $query = self::$connect->query($sql);
+        // $connect= $query->execute();
+        if ($query!=false) {
+            echo "ADD Success!";
+        } else {
+            echo "ERROR" . $sql . "<br>";
+        }
+    }
+        
+    //pour supprimer dans notre liste
+     static function dropProd()
+     {
+         self::createConnexion();
+         $sql="DELETE FROM `produit` WHERE nom = 'test6'";
+         $query = self::$connect->query($sql);
+         if ($query!=false) {
+            echo "DROP Success!";
+        } else {
+            echo "ERROR" . $sql . "<br>";
+        }
+     }
+
+    */
     /* if (isset($_POST['sub_btn']))
             {
                 self::createConnexion();
                 if(!mysql_query("INSERT INTO produit(id, nom, prix_unit) VALUES ('', 'test','5euro')") )
                 die ("Verifier la connexion");
             }
-    */
+                */
 }
-
-
+database:: createConnexion();
+database:: chooseShop();
+//database:: addProd();
+//database:: dropProd();
--- index.php
+++ index.php
@@ -9,14 +9,24 @@
                 background: rgba(0, 128, 0, 0.2);
             }
             input[type=text] {
-                width: 100%;
-                height: 150px;
+                width: 50%;
+                height: 20px;
                 padding: 12px 20px;
-                box-sizing: border-box;
+                box-sizing: content-box;
                 border: 2px solid #ccc;
                 border-radius: 4px;
                 background-color: #f8f8f8;
                 resize: none;
+            }
+            select{
+                width: 50%;
+                height: 20px;
+                padding: 12px 20px;
+                box-sizing: content-box;
+                border: 2px solid #ccc;
+                border-radius: 4px;
+                background-color: #f8f8f8;
+                resize:both;
             }
             button{
                 background-color: white;
@@ -38,15 +48,65 @@
 	</head>
 	<body>
     <h1>Liste des courses</h1>
-	<?php
+	
+    <!--
+        <?php
         require_once('database.php');
-        $list= database::getList();
-        var_dump($list);
-    ?>
-        <p>Click "Valider" pour afficher votre liste</p>
+        $data = database::getList();
+        var_dump($data);
+        ?> 
+    
+    
+        <p> Trouvez votre "Magasin" et appuyez "Valider" pour le selectionner</p>
         <form method="post" action="">
-            <input type="text" id="liste" name="liste" required /><br/>
+            <input type="list" id="magasin" name="magasin" required /><br/>
             <button type="submit">Valider</button>
 		</form>
-	</body>
+	-->
+    
+    <h2>Votre magasin</h2>
+    <form method ="post">
+     
+      <select name="magasin" id="magasin">
+          <?php
+          require_once('database.php');
+          $data = database::chooseShop();
+           foreach ($data as $row):
+          ?>
+            <option><?=$row["nom"]?></option>
+          <?php endforeach ?>
+         
+           <form method="post" action="">
+            <input type="text" id="nmagasin" name="nmagasin" value="Entrez ici un magasin à ajouter" required /><br/>
+            <button type="submit">Valider</button>
+           </form>
+      
+      </select>
+      
+    <h2>Vos produits</h2>
+    
+      <select name="produits" id="produits">
+           <?php
+           require_once('database.php');
+           $data = database::getList();
+            foreach ($data as $row): 
+           ?>
+             <option><?=$row["nom"]?></option>
+           <?php endforeach ?>
+      </select>
+    
+    <form method="post" action="">
+            <input type="text" id="nproduit" name="nProduit" value="Entrez ici un produit à ajouter" required /><br/>
+            <button type="submit">Valider</button>
+    </form>
+
+    <form>
+    <input type="text" id="dproduit" name="dProduit" value="Entrez ici le produit à supprimer" required /><br/>
+            <button type="submit">Valider</button>
+    </form>
+
+    </form>
+    </body>
+
+    
 </html>
